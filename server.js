44541e4804c4f7c4eab7c6749c40c85300d72006
HuangKaiFeng <73734562+DaybreakAshe@users.noreply.github.com>
2024-10-08 15:05:24 +0800

    update code

--- server.js
+++ server.js
@@ -2,8 +2,17 @@
 const express = require("express");
 const app = express();
 
+// 定义一个简单的路由
 app.get("/api/hello", (req, res) => {
     res.json({ message: "Hello from Express!" });
+});
+
+// 设置端口，默认使用 3999
+const PORT = 3999;
+
+// 启动服务器，监听指定的端口
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 module.exports = app;
